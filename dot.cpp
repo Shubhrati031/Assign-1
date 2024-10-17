16c337a4ba0956a5521998ab1dca810b2e05f29c
Gunjan <2023ucp1604@mnit.ac.in>
2024-10-17 12:05:51 +0530

    dot product file added

---
+++ dot.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int xi,xj,yi,yj;
+    cout<<"enter vector 1:";
+    cin>>xi>>xj;
+    cout<<"enter vector 2:";
+    cin>>yi>>yj;
+    cout<<"dot product of the vectors is: "<<(xi*yi)+(xj*yj);
+}
