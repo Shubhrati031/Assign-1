6cf1853b7aa4e925db6382191d32dd677ba634af
Gunjan <2023ucp1604@mnit.ac.in>
2024-10-17 12:10:53 +0530

    file is modified

---
+++ dot.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int xi,xj,yi,yj;
+    cout<<"enter vector 1:";
+    cin>>xi>>xj;
+    cout<<"enter vector 2:";
+    cin>>yi>>yj;
+    cout<<"dot product of the vectors is: "<<(xi*yi)+(xj*yj)<<endl;
+    cout<<"newline added"<<endl;
+}
